f80e63fdb7414406053e4430e1797dc28706da65
vaghaav <vagharshak.avetisyan@yahoo.com>
2022-07-26 21:03:32 +0400

    Refactored recursive method to return path count.

--- src/FindPathCount/Main.java
+++ src/FindPathCount/Main.java
@@ -1,8 +1,5 @@
 package FindPathCount;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     private static int count = 0;
@@ -12,34 +9,33 @@ public class Main {
         // write your code here
         int[][] array = {
 
-                {1, 1, 1},
-
                 {1, 0, 1},
+
+                {1, 0, 0},
 
                 {1, 1, 1}
 
         };
-        new Main().findPathsCount(array, 0, 0);
-        System.out.println(count);
+
+        System.out.println(findPathsCount(array, 0, 0));
     }
 
-    private void findPathsCount(int[][] mat, int i, int j) {
+    private static int findPathsCount(int[][] mat, int i, int j) {
         int m = mat.length - 1;
         int n = mat[0].length - 1;
 
         if (i > m || j > n) {
-            return;
+            return 0;
         }
 
         if (mat[i][j] != 1) {
-            return;
-        }
-        
-        if (i == m && j == n) {
-            this.count++;
+            return 0;
         }
 
-        findPathsCount(mat, i + 1, j);
-        findPathsCount(mat, i, j + 1);
+        if (i == m && j == n) {
+            return 1;
+        }
+
+        return findPathsCount(mat, i + 1, j) + findPathsCount(mat, i, j + 1);
     }
 }
